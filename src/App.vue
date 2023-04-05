0477ab180be038e969929e5a827434015e3b12cb
BYRA Bastien <bbyra@u-bordeaux.fr>
2023-04-05 13:35:12 +0200

    ajout champ de recherche

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <div class="ml-8 w-25">
+  <label>Chercher un film</label>
+  <v-text-field label="Nom du film, auteur, date de parution..." v-model="search" type="text"/>
+  <br><br>
+  </div>
+
+
   <div class="d-flex"> 
-    <MovieItem :listMovie="listMovie"/>
+    <MovieItem :listMovie="movieSearched"/>
   </div>
 
 </template>
@@ -16,20 +23,42 @@ export default {
   },
   data: function() {
     return {
-      listMovie: ["Truc 1", "Truc 2"],
+      listMovie: [
+        {
+          title: "Name mov",
+          parution_date: 1998,
+          language: "English",
+          genres: ["Science", "Drama"],
+          creator: {
+            firstname: "Jack",
+            lastname: "DUPOND"
+          }
+        }
+      ],
       search: ""
     }
-  }
+  },
+  computed: {
+    movieSearched() {
+        return this.listMovie.filter(movie => movie.title.toLowerCase().includes(this.search.toLowerCase()) ||
+        movie.parution_date.toString().includes(this.search) ||
+        movie.creator.firstname.toLowerCase().includes(this.search.toLowerCase()) ||
+        movie.creator.lastname.toLowerCase().includes(this.search.toLowerCase()))
+    }
+  },
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+#app {
+  margin-top: 20px;
 }
 </style>
--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,43 +1,19 @@
 <template>
-    <!-- <v-col cols="12" sm="3" v-for="movie in listMovie" v-bind:key="movie">
-            <div>
-                <img class="poster" src="https://marketplace.canva.com/EAFH3gODxw4/1/0/1131w/canva-black-%26-white-modern-mystery-forest-movie-poster-rLty9dwhGG4.jpg"/>
-            </div>
-            <div>
-                <h1>Title</h1>
-                <p>Date de parution</p>
-                <p>Réalisateur</p>
-                <a>Lien de detail</a>
-                <h2>{{ movie }}</h2>
-            </div>
-        </v-col> -->
         <div class="px-6" v-for="movie in listMovie" v-bind:key="movie">
             <div>
                 <img class="poster" src="https://marketplace.canva.com/EAFH3gODxw4/1/0/1131w/canva-black-%26-white-modern-mystery-forest-movie-poster-rLty9dwhGG4.jpg"/>
             </div>
             <div>
-                <h1>Title</h1>
-                <p>Date de parution</p>
-                <p>Réalisateur</p>
+                <h1>{{ movie.title }}</h1>
+                <p>{{ movie.creator.lastname }} {{ movie.creator.firstname }}</p>
+                <p>{{ movie.parution_date }}, {{ movie.language }}</p>
+                <!-- <p>Genres : {{ concat_genre(movie.genres) }}</p> -->
                 <a>Lien de detail</a>
-                <h2>{{ movie }}</h2>
             </div>
         </div>
-
 </template>
 
 <script>
-// import { defineComponent } from '@vue/composition-api'
-
-// export default defineComponent({
-//     setup() {
-        
-//     },
-//     props: {
-//         listMovie: []
-//     }
-// })
-
 export default {
     props: {
         listMovie: []
@@ -47,6 +23,13 @@ export default {
       
       }
     },
+    computed: {
+        concat_genre(array_genres) {
+            console.log(array_genres[0])
+            // return "truc"
+            return array_genres.join(', ')
+        }
+    }
 }
 </script>
 
